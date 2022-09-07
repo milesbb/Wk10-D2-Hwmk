877e38c8f774f6ad8355442f0c54a06c2fde2133
Miles Bailey-Braendgaard <milesjbb@gmail.com>
2022-09-07 13:50:30 +0100

    removed useless imports

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
-import logo from './logo.svg';
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
-import WarningSign from './components/WarningSign';
-import MyBadge from './components/MyBadge';
 import BookList from './components/BookList';
 import MyNav from './components/MyNav';
-// import books from "./books/history.json";
 
 function App() {
   return (
     <div className="App">
       <MyNav />
       <h1>Book Finder</h1>
-      {/* <WarningSign text="Test text" /> */}
       <BookList />
     </div>
   );
